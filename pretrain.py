d5f46e02c8ad49c1aaf5ce4fea87c78e2cab6655
euzmin <qq1227112820@icloud.com>
2025-05-19 21:43:05 +0800

    适配远程服务器

--- pretrain.py
+++ pretrain.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
     args = TrainingArguments(output_dir='./results/', num_train_epochs=10,
                              do_train=True,
                              # 每个设备（GPU）上的 batch size：假如你有两个 GPU，总的 batch size 是 128 × 2 = 256。
-                             per_device_train_batch_size=32,
+                             per_device_train_batch_size=48,
                              # 梯度累积步数：等价于将 batch size 放大 8 倍（模拟大 batch）。
                              # 实际每 8 个小 batch 才执行一次 optimizer.step()。
                              # 累积期间只做 loss.backward()，第 N 步才 optimizer.step()
                              # 有效 batch size = 
                              # per_device_train_batch_size × num_devices × gradient_accumulation_steps
-                             gradient_accumulation_steps=16,
+                             gradient_accumulation_steps=8,
                              # 如果设置了这个值，将优先按步数停止，而不是 num_train_epochs。
                              # max_steps=15000,
                              # 每训练 100 步，打印一次日志
