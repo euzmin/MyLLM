37f221aa62bdbac8a89b82ac6948c49cbbe3c70a
euzmin <qq1227112820@icloud.com>
2025-05-19 17:38:05 +0800

    拆分了myllm的代码，把pretrain提出来了

--- pretrain.py
+++ pretrain.py
@@ -1,37 +1,52 @@
-from transformers import PretrainedConfig
-
-# # 编写自定义配置时需要记住的2个重要事项如下：
-# 1. 必须继承自 PretrainedConfig
-# 2. 自定义的配置中 super().__init__() 必须能接受任意数量的参数，即**kwargs
-class Config(PretrainedConfig):
-    def __init__(self, 
-                 hidden_size = 512,
-                 num_attention_heads = 16,
-                 num_key_value_heads = 8, #?
-                 flash_attn = True, #?
-                 attention_bias = False, #?
-                 max_seq_len = 512, #?
-                 intermediate_size = 2048, #?
-                 mlp_bias = False,
-                 vocab_size = 6400, #?
-                 n_layers = 8,
-                 dropout = 0.0,
-                 **kwargs):
-        super().__init__(**kwargs)
-        self.hidden_size = hidden_size
-        self.num_attention_heads = num_attention_heads
-        self.num_key_value_heads = num_key_value_heads
-        self.flash_attn = flash_attn
-        self.attention_bias = attention_bias
-        self.max_seq_len = max_seq_len
-        self.intermediate_size = intermediate_size
-        self.mlp_bias = mlp_bias
-        self.vocab_size = vocab_size
-        self.n_layers = n_layers
-        self.dropout = dropout
-        super().__init__(**kwargs)
+from models.myllm import Config, MyLLM
+from transformers import DefaultDataCollator, AutoTokenizer
+from transformers import TrainingArguments, Trainer
+from data.utils import LLMDataset
 
 if __name__ == '__main__':
     config = Config()
+    model = MyLLM(config)
+    print(f'模型参数量为：{sum(param.numel() for param in model.parameters() if param.requires_grad)}')
 
+    data_collator = DefaultDataCollator()
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = 'left'
+    args = TrainingArguments(output_dir='./results/', num_train_epochs=10,
+                             do_train=True,
+                             # 每个设备（GPU）上的 batch size：假如你有两个 GPU，总的 batch size 是 128 × 2 = 256。
+                             per_device_train_batch_size=8,
+                             # 梯度累积步数：等价于将 batch size 放大 8 倍（模拟大 batch）。
+                             # 实际每 8 个小 batch 才执行一次 optimizer.step()。
+                             # 累积期间只做 loss.backward()，第 N 步才 optimizer.step()
+                             # 有效 batch size = 
+                             # per_device_train_batch_size × num_devices × gradient_accumulation_steps
+                             gradient_accumulation_steps=8,
+                             # 如果设置了这个值，将优先按步数停止，而不是 num_train_epochs。
+                             # max_steps=15000,
+                             # 每训练 100 步，打印一次日志
+                             logging_steps=100,
+                             report_to='tensorboard',
+                             # 最多保留几个 checkpoint
+                             save_total_limit=5,
+                             # 使用 bfloat16 精度训练
+                             bf16=True,
+                             learning_rate=2e-4,
+                             lr_scheduler_type='cosine',
+                             # 使用 8 个进程/线程预处理数据，加快数据读取速度。
+                             dataloader_num_workers=8,
+                             # 是否将数据固定在 CUDA 的 page-locked memory 中（加快数据传输速度）
+                             dataloader_pin_memory=True,
+                             # 是否保存为 .safetensors 格式：
+                             # 设为 False 表示仍使用传统的 PyTorch .bin 格式。
+                             save_safetensors=False
+                             )
+    # 注意，dataset 和 trainer 里 tokenizer 的作用不同
+    # dataset 里就是用来分词，得到 token ID
+    dataset = LLMDataset('D:\code\MyLLM\data\pretrain_hq.jsonl', tokenizer=tokenizer, max_seq_len=512)
+    # trainer 里 tokenizer 用于 日志打印、模型保存，由token ID 解码到自然语言打印出来等目的
+    trainer = Trainer(model=model, args=args, train_dataset=dataset, tokenizer=tokenizer, data_collator=data_collator)
 
+    trainer.train(resume_from_checkpoint=False)
+    trainer.save_model('./results/saved_model')
+    trainer.save_state()
