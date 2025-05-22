7da36c9fed87679eb728bba623e56f6cf2471d9b
euzmin <qq1227112820@icloud.com>
2025-05-22 15:36:10 +0800

    修改bug

--- data/utils.py
+++ data/utils.py
@@ -28,7 +28,7 @@ class PreTrainDataset(Dataset):
             add_special_tokens=False,
             truncation=True,
             max_length=self.max_seq_len,
-            padding='max_length'  # 可选：也可以用 pad 到 max_seq_len
+            padding='max_length'  
         )
         # input_ids = self.tokenizer.encode(text, add_special_tokens=False)
         input_ids = tokens['input_ids']
@@ -90,14 +90,16 @@ class SFTDataset(Dataset):
             prompt,
             add_special_tokens=False,
             truncation=True,
-            max_length=self.max_seq_len // 2  # 可调，避免 prompt+answer 总长超限
+            max_length=self.max_seq_len // 2,  # 可调，避免 prompt+answer 总长超限
+            padding='max_length'
         )
 
         answer_ids = self.tokenizer.encode(
             answer,
             add_special_tokens=False,
             truncation=True,
-            max_length=self.max_seq_len // 2
+            max_length=self.max_seq_len // 2,
+            padding='max_length'
         )
         
         vocab_size = self.tokenizer.vocab_size
@@ -111,22 +113,21 @@ class SFTDataset(Dataset):
         # 这里要和llm的损失函数对应，ignore_index应是-100
         labels = [-100] * len(prompt_ids) + answer_ids
 
-        # 截断或 padding
-        if len(input_ids) > self.max_seq_len:
-            input_ids = input_ids[:self.max_seq_len]
-        else:
-            pad_len = self.max_seq_len - len(input_ids)
-            input_ids += [self.tokenizer.pad_token_id] * pad_len
-            
+        # # 截断或 padding
+        # if len(input_ids) > self.max_seq_len:
+        #     input_ids = input_ids[:self.max_seq_len]
+        # else:
+        #     pad_len = self.max_seq_len - len(input_ids)
+        #     input_ids += [self.tokenizer.pad_token_id] * pad_len
         
-        if len(labels) > self.max_seq_len:
-            labels = labels[:self.max_seq_len]
-        else:
-            pad_len = self.max_seq_len - len(labels)
-            labels += [-100] * pad_len
+        # if len(labels) > self.max_seq_len:
+        #     labels = labels[:self.max_seq_len]
+        # else:
+        #     pad_len = self.max_seq_len - len(labels)
+        #     labels += [-100] * pad_len
 
-        input_ids = torch.tensor(input_ids, dtype=torch.long)
-        labels = torch.tensor(labels, dtype=torch.long)
+        input_ids = torch.tensor(input_ids[:-1], dtype=torch.long)
+        labels = torch.tensor(labels[1:], dtype=torch.long)
 
         return {
             "input_ids": input_ids,
